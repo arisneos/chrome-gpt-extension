edc9479eb65302a3fd20cd24d7645c1bdf947f77
ArisNeos <arisneos@gmail.com>
2023-07-20 15:03:06 +0300

    Migrated from other project
    Minor changes in usability

---
+++ popup.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Send a message to the content script to get the selected text
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "getSelectedText" }, function (response) {
+      var highlightedText = response ? response.text : "";
+      if (highlightedText) {
+        document.getElementById("highlightedText").innerText = highlightedText;
+
+        // Add event listener to the copy button
+        var copyButton = document.getElementById("copyButton");
+        copyButton.addEventListener("click", function () {
+          copyToChatGPT(highlightedText);
+        });
+      } else {
+        document.getElementById("highlightedText").innerText = "No text highlighted.";
+      }
+    });
+  });
+});
+
+function copyToChatGPT(text) {
+  // Write the highlighted text to the clipboard
+  navigator.clipboard.writeText(text).then(function () {
+    alert("Highlighted text copied to Chat GPT's chat!");
+  });
+}
+
+// Listen for the keyboard shortcut and trigger the copy action
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "copy_to_chat_gpt") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "getSelectedText" }, function (response) {
+        var highlightedText = response ? response.text : "";
+        if (highlightedText) {
+          copyToChatGPT(highlightedText);
+        }
+      });
+    });
+  }
+});
